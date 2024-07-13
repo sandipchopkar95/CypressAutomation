877308468e9f5e871628def34087ea5197e64eec
Sandip Chopkar <sandipchopkar789@gmail.com>
2024-07-14 03:35:52 +0530

    iterator clikc

--- cypress/e2e/examples/Test1.js
+++ cypress/e2e/examples/Test1.js
@@ -5,7 +5,18 @@ describe('My first test Suite', function () {
         cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/")
         cy.get('form input.search-keyword').type('ca')
         cy.wait(2000)
-        cy.get('div.products .product').should('have.length', 4)
+        cy.get('.product:visible').should('have.length', 4)
+        //parent child chaining
+        cy.get('.products').find('.product').should('have.length', 4)
+        cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click()
+
+        //parent child with itertor
+        cy.get('.products').find('.product').each(($el, index, $list) => {
+            const textVeg = $el.find('h4.product-name').text()
+            if (textVeg.includes('Cashews')) {
+                cy.wrap($el).find('button').click()
+            }
+        })
      
     })
 
