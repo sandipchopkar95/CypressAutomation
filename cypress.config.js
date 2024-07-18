13519c05cb8138fe5a709a6eccdaa0c7606ed496
Sandip Chopkar <sandipchopkar789@gmail.com>
2024-07-18 17:06:01 +0530

    Report

--- cypress.config.js
+++ cypress.config.js
@@ -2,12 +2,19 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
 
+  defaultCommandTimeout:6000,
+  reporter: 'cypress-mochawesome-reporter',
+
   env: {
     url: "https://rahulshettyacademy.com"
   },
+  retries: {
+    runMode: 1,
+    },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
     specPattern: 'cypress/e2e/examples/*.js'
   },
